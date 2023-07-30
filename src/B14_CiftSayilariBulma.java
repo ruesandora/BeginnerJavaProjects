d0b35de795f21840b7f5d8b5e6f7b63e65af1797
rues <ruesforum@gmail.com>
2023-07-30 18:23:35 +0300

    G

--- src/B14_CiftSayilariBulma.java
+++ src/B14_CiftSayilariBulma.java
@@ -17,39 +17,37 @@ public class B14_CiftSayilariBulma {
 
     System.out.print("Ortalamasını hesaplanmasını istediğiniz sayıyı giriniz: ");
     int number1 = scan.nextInt();
+    System.out.println();
     int dordunKati = 0;
     int besinKati = 0;
     int sayac1 = 0;
     int sayac2 = 0;
-    int output1 = 0;
-    int output2 = 0;
 
-    for (int i = 0; i <= number1; i++) {
+    for (int i = 1; i <= number1; i++) {
       if (i % 4 == 0){
         dordunKati += i;
-        sayac1 += 1;
+        sayac1++;
       }
       if (i % 5 == 0){
         besinKati += i;
-        sayac2 += 1;
+        sayac2++;
       }
-
-      output1 = (dordunKati / sayac1);
-      output2 = (besinKati / sayac2);
     }
 
-    sayac1--;
-    sayac2--;
+    double output1 = (double) dordunKati / sayac1;
+    double output2 = (double) besinKati / sayac2;
 
-    System.out.println(dordunKati);
-    System.out.println(sayac1);
-    System.out.println(output1);
+    // 4'ün bölünenleri hakkında çıktılar:
+    System.out.println("- Sayınızın dörde bölünenlerin toplamı: " + dordunKati);
+    System.out.println("- Sayınız toplamda " + sayac1 + " kez dörde bölünüyor..");
+    System.out.println("- Sayınızın tam bölünen sayıların ortalaması: " + output1);
 
     System.out.println();
 
-    System.out.println(besinKati);
-    System.out.println(sayac2);
-    System.out.println(output2);
+    // 5'in bölünenleri hakkında çıktılar:
+    System.out.println("- Sayınızın beşe bölünenlerin toplamı: " + besinKati);
+    System.out.println("- Sayınız toplamda " + sayac2 + " kez beşe bölünüyor..");
+    System.out.println("- Sayınızın tam bölünen sayıların ortalaması: " + output2);
 
   }
 
