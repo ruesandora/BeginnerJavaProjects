127ce3d459232fa6e9d265f12db89dfcd9acf8ba
rues <ruesforum@gmail.com>
2023-07-30 18:09:53 +0300

    G

--- src/B14_CiftSayilariBulma.java
+++ src/B14_CiftSayilariBulma.java
@@ -4,16 +4,54 @@ public class B14_CiftSayilariBulma {
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
-    System.out.print("Lütfen bir tamsayi giriniz: ");
-    int number = scan.nextInt();
+//    System.out.print("Lütfen bir tamsayi giriniz: ");
+//    int number = scan.nextInt();
+//
+//    for (int i = 1; i <= number; i++) {
+//      if (i % 2 == 0){
+//        System.out.println(i + ": Sayısı çift");
+//      }
+//    }
 
-    for (int i = 1; i <= number; i++) {
-      if (i % 2 == 0){
-        System.out.println(i + ": Sayısı çift");
+// Java ile 0'dan girilen sayıya kadar olan sayılardan 4 ve 5'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
+
+    System.out.print("Ortalamasını hesaplanmasını istediğiniz sayıyı giriniz: ");
+    int number1 = scan.nextInt();
+    int dordunKati = 0;
+    int besinKati = 0;
+    int sayac1 = 0;
+    int sayac2 = 0;
+    int output1 = 0;
+    int output2 = 0;
+
+    for (int i = 0; i <= number1; i++) {
+      if (i % 4 == 0){
+        dordunKati += i;
+        sayac1 += 1;
       }
+      if (i % 5 == 0){
+        besinKati += i;
+        sayac2 += 1;
+      }
+
+      output1 = (dordunKati / sayac1);
+      output2 = (besinKati / sayac2);
     }
 
+    sayac1--;
+    sayac2--;
 
+    System.out.println(dordunKati);
+    System.out.println(sayac1);
+    System.out.println(output1);
+
+    System.out.println();
+
+    System.out.println(besinKati);
+    System.out.println(sayac2);
+    System.out.println(output2);
 
   }
+
+  // Kodu daha basit  ve okunur hale getirelim.
 }
